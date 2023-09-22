805865647fe6b36f1727cd29df93028da94df3f8
VanNgo1610 <142959206+VanNgo1610@users.noreply.github.com>
2023-09-22 09:45:20 +0700

    Update test 1.c

--- test 1.c
+++ test 1.c
@@ -9,19 +9,19 @@ float divine(float a, float b);
 int main() {
 	
 	float a, b;
-	printf("\nNhap so 1: ");
+	printf("\nEnter the first number: ");
 	scanf("%f", &a);
-	printf("\nNhap so 2: ");
+	printf("\nEnter the second number: ");
 	scanf("%f", &b);
 	
-	int so1 = sum(a,b);
-	printf("\nsum = %d", so1);
-	int so2 = subtract(a, b);
-	printf("\nsubtract = %d", so2);
-	int so3 = mutiply(a, b);
-	printf("\nmutiply = %d", so3);
-	float so4 = divine(a, b);
-	printf("\ndivine = %.1f", so4);
+	int num1 = sum(a,b);
+	printf("\nsum = %d", num1);
+	int num2 = subtract(a, b);
+	printf("\nsubtract = %d", num2);
+	int num3 = mutiply(a, b);
+	printf("\nmutiply = %d", num3);
+	float num4 = divine(a, b);
+	printf("\ndivine = %.1f", num4);
 	
 	return 0;
 }
@@ -30,8 +30,8 @@ int main() {
 int sum(int a, int b) {
 	int c;
 	printf("\nSum:\n");
-	printf("\nSo thu nhat la: %d", a);
-	printf("\nSo thu hai la : %d", b);
+	printf("\nThe first number: %d", a);
+	printf("\nThe second number: %d", b);
 	c = a + b;
 	return c;
 }
@@ -39,8 +39,8 @@ int sum(int a, int b) {
 int subtract(int a, int b) {
 	int c;
 	printf("\nSubtraction:\n");
-	printf("\nSo thu nhat la: %d", a);
-	printf("\nSo thu hai la : %d", b);
+	printf("\nThe first number: %d", a);
+	printf("\nThe second number: %d", b);
 	c = a - b;
 	return c;
 }
@@ -48,8 +48,8 @@ int subtract(int a, int b) {
 int mutiply(int a, int b) {
 	int c;
 	printf("\nMultiply:\n");
-	printf("\nSo thu nhat la: %d", a);
-	printf("\nSo thu hai la : %d", b);
+	printf("\nThe first number: %d", a);
+	printf("\nThe second number: %d", b);
 	c = a * b;
 	return c;
 }
@@ -57,8 +57,8 @@ int mutiply(int a, int b) {
 float devide(float a, float b) {
 	float c;
 	printf("\nDivision:\n");
-	printf("\nSo thu nhat la: %f", a);
-	printf("\nSo thu hai la : %f", b);
+	printf("\nThe first number: %f", a);
+	printf("\nThe second number: %f", b);
 	c = a / b;
 	return c;
 }
